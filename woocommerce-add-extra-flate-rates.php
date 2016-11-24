062c86dc3c922d824edf85b227a23eac4b3726d6
Gunter <gunter@dressyourwp.com>
2016-11-24 15:59:09 -0300

    Updates

--- woocommerce-add-extra-flate-rates.php
+++ woocommerce-add-extra-flate-rates.php
@@ -1,9 +1,8 @@
 <?php
 
 
-	add_filter( 'woocommerce_enable_deprecated_additional_flat_rates', '__return_true' );
 
-//add_action( 'woocommerce_flat_rate_shipping_add_rate', 'add_another_custom_flat_rate', 10, 2 );
+add_action( 'woocommerce_flat_rate_shipping_add_rate', 'add_another_custom_flat_rate', 10, 2 );
 
 function add_another_custom_flat_rate( $method, $rate ) {
 	$new_rate          = $rate;
@@ -12,17 +11,5 @@ function add_another_custom_flat_rate( $method, $rate ) {
 	$new_rate['cost']  += 13; // Add $2 to the cost
 
 
-
-}
-//add_action( 'woocommerce_flat_rate_shipping_add_rate', 'next_day_flat_rate', 10, 1);
-
-function next_day_flat_rate( $method, $rate ) {
-	$next_day          = $rate;
-	$next_day['id']    .= ':' . 'next_day'; // Append a custom ID
-	$next_day['label'] = 'Next day delivery'; // Rename to 'Rushed Shipping'
-	$next_day['cost']  += 30; // Add $2 to the cost
-
-	// Add it to WC
-	$method->add_rate( $next_day );
 
 }
